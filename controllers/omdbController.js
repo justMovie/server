deab5409fb9d7431246c37649024cf137ee6b2e1
michaelwp <michael.wenceslaus@gmail.com>
2020-01-10 17:17:13 +0700

    final-checkout

--- controllers/omdbController.js
+++ controllers/omdbController.js
@@ -12,7 +12,22 @@ class omdbController {
         }).then(response => {
             res.status(200).json(response.data)
         }).catch(err => {
-            res.json(err.message)
+            res.json(err)
+        })
+    }
+
+    static getMovieDetails(req, res, next) {
+        axios({
+            method: 'get',
+            url: `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${req.params.title}`,
+            headers: {
+                'X-RapidAPI-Host': 'movie-database-imdb-alternative.p.rapidapi.com',
+                'X-RapidAPI-Key': '11924a55bbmshd7bf0fb52ffcb41p1c3effjsnec52571df969'
+            }
+        }).then(response => {
+            res.status(200).json(response.data)
+        }).catch(err => {
+            res.json(err)
         })
     }
 }
