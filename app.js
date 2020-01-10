a4ba3ec5415b09c79e68423c57ef1f2d1dae00a8
Immanuel Pandiangan <nuelpan@gmail.com>
2020-01-10 12:37:51 +0700

    add use routers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routers = require('./routers')
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
-app.use(routers)
+app.use('/', routers)
 app.use(errorHandler);
 
 module.exports = app;
